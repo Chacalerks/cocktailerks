146bd62cc2603982e911ad59bfc33b4f4ce133ff
Chacalerks <erksmartinez2014@gmail.com>
2024-02-29 23:13:21 -0600

    Creating a grid to show

--- src/components/features/CocktailList.js
+++ src/components/features/CocktailList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, TextField, Grid, Card, CardMedia, CardContent, Typography } from '@mui/material';
 
 const CocktailList = ({ onCocktailSelect }) => {
     const [cocktails, setCocktails] = useState([]);
@@ -18,14 +18,38 @@ const CocktailList = ({ onCocktailSelect }) => {
     }, [inputValue]);
 
     return (
-        <Autocomplete
-            freeSolo
-            options={cocktails}
-            getOptionLabel={(option) => option.strDrink}
-            renderInput={(params) => <TextField {...params} label="Search Cocktails" variant="outlined" />}
-            onInputChange={(_, newInputValue) => setInputValue(newInputValue)}
-            onChange={(_, newValue) => onCocktailSelect(newValue)}
-        />
+        <>
+            <Autocomplete
+                freeSolo
+                options={cocktails}
+                getOptionLabel={(option) => option.strDrink}
+                renderInput={(params) => <TextField {...params} label="Search Cocktails" variant="outlined" />}
+                onInputChange={(_, newInputValue) => setInputValue(newInputValue)}
+                onChange={(_, newValue) => onCocktailSelect(newValue)}
+            />
+            <Grid container spacing={2} sx={{ marginTop: 2 }}>
+                {cocktails.map((cocktail) => (
+                    <Grid item xs={12} sm={6} md={4} key={cocktail.idDrink}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image={cocktail.strDrinkThumb}
+                                alt={cocktail.strDrink}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    {cocktail.strDrink}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {cocktail.strAlcoholic}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </>
     );
 };
 
